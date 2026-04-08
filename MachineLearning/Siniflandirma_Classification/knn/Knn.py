eeebdac68052368183bd8dadae843f3cc0c82d90
İlknur Demiral <nurdemiral11@gmail.com>
2026-04-08 19:45:43 +0300

    Add files via upload

---
+++ MachineLearning/Siniflandirma_Classification/knn/Knn.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 17:36:34 2026
+
+@author: ilknur
+"""
+
+#knn
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+
+veriler = pd.read_csv('veriler.csv', sep=";")
+print(veriler)
+
+
+x= veriler.iloc[:,1:4].values #bağımsız değişken
+y = veriler.iloc[:,4:].values#bağımlı değişken
+print(y)
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors =1, metric='minkowski')
+knn.fit(X_train,y_train)
+
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
