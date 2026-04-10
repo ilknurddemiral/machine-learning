6fa47a8f288dbee7331468bc9c839dc91ce2f17f
İlknur Demiral <nurdemiral11@gmail.com>
2026-04-10 22:03:30 +0300

    Add files via upload

---
+++ MachineLearning/Siniflandirma_Classification/SVM/SVM.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 27 15:12:41 2026
+
+@author: ilknur
+"""
+
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+
+
+veriler = pd.read_csv('veriler.csv', sep=";")
+print(veriler)
+
+
+x= veriler.iloc[:,1:4].values #bağımsız değişken
+y = veriler.iloc[:,4:].values#bağımlı değişken
+print(y)
+
+#verilerin egitim ve test icin bolunmesi
+from sklearn.model_selection import train_test_split
+
+x_train, x_test,y_train,y_test = train_test_split(x,y,test_size=0.33, random_state=0)
+
+#verilerin olceklenmesi
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors =1, metric='minkowski')
+knn.fit(X_train,y_train)
+
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+#SVC
+from sklearn.svm import SVC
+svc = SVC(kernel='rbf')
+svc.fit(X_train,y_train)
+
+y_pred = svc.predict(X_test)
+
+cm = confusion_matrix(y_test,y_pred)
+print('SVC')
+print(cm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
